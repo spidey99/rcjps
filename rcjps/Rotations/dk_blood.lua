132f6add47e4c0970037e193155ad9fe860fb066
rjones <robert.conrad.jones@gmail.com>
2013-10-15 15:01:16 -0500

    update prot pallly and blood dk

--- rcjps/Rotations/dk_blood.lua
+++ rcjps/Rotations/dk_blood.lua
@@ -29,7 +29,9 @@ function new_dk_blood(self)
 	local bcStacks = jps.buffStacks("blood charge") --Blood Stacks
 	
 	local haveGhoul, _, _, _, _ = GetTotemInfo(1) --Information about Ghoul pet
-
+	
+	local myHP = jps.hp()
+	
 	local dr1 = select(3,GetRuneCooldown(1))
 	local dr2 = select(3,GetRuneCooldown(2))
 	local ur1 = select(3,GetRuneCooldown(3))
@@ -43,45 +45,100 @@ function new_dk_blood(self)
 	local one_ur = ur1 or ur2
 	local two_ur = ur1 and ur2
 	
-	local spellTable =
-	{
-		-- Blood presence
-		{ "blood presence", 		not jps.buff("blood presence") },
-		-- Moved DnD to the top so it will cast immediately
-		{ "death and decay",		IsShiftKeyDown() ~= nil and GetCurrentKeyBoardFocus() == nil }, -- GetCurrentKeyBoardFocus: Avoid casting while chat is open and you press shift
-		{ "army of the dead",		IsLeftControlKeyDown() ~= nil and GetCurrentKeyBoardFocus() == nil }, -- todo
-		-- Taunt
-		{ "dark command", 			targetThreatStatus ~= 3 and not jps.targetTargetTank() },
-		-- Kick
-		{ "mind freeze", 			jps.shouldKick() },
-		{ "Strangulate", 			jps.shouldKick() and jps.LastCast ~= "mind freeze" },
-		-- Aggro cooldowns
-		{ "Raise Dead", 			jps.UseCDs and UnitExists("pet") == nil },
-		{ "Dancing Rune Weapon", 	jps.UseCDs },
-		-- Defensive cooldowns
-		{ "Death Pact", 			jps.hp() < 0.5 and haveGhoul},
-		{ "Icebound Fortitude", 	jps.hp() < 0.3 },
-		{ "Vampiric Blood", 		jps.hp() < 0.5 },
-		{ "Rune Tap", 				jps.hp() < 0.8 },
-		-- Trinkets
-      	{ jps.useTrinket(1),    jps.UseCds },
-      	{ jps.useTrinket(2),    jps.UseCds },		
-		-- Buffs
-		{ "Bone Shield", 			not jps.buff("bone shield") },
-		-- Single target
-		{ "outbreak",				ffDuration <= 2 or bpDuration <= 2 },
-		{ "soul reaper",			jps.hp("target") <= 0.35 }, 					-- Requires level 87
-		{ "plague strike", 			not jps.debuff("blood plague") },
-		{ "icy touch", 				not jps.debuff("frost fever") },
-		{ "pestilence", 			jps.MultiTarget },
-		{ "death strike", 			},
-		{ "blood boil", 			jps.buff("crimson scourge") },
-		{ "heart strike", 			jps.debuff("blood plague") and jps.debuff("frost fever") },
-		{ "rune strike", 			rp >= 40 },
-		{ "horn of winter", 		},
-		{ "empower rune weapon" , 	not two_dr and not two_fr and not two_ur },
-		{ {"macro","/startattack"}, nil, "target" },
-   	}
+	if(jps.MultiTarget) then
+		local spellTable =
+		{
+			-- Blood presence
+			{ "blood presence", 		not jps.buff("blood presence") },
+			
+			{ "blood tap", 				"onCD" },
+			
+			-- Moved DnD to the top so it will cast immediately
+			{ "death and decay",		one_dr or one_ur }, -- GetCurrentKeyBoardFocus: Avoid casting while chat is open and you press shift
+			
+			{ "army of the dead",		IsLeftControlKeyDown() ~= nil and GetCurrentKeyBoardFocus() == nil }, -- todo
+			-- Taunt
+			{ "dark command", 			IsShiftKeyDown() ~= nil and (targetThreatStatus ~= 3 or not jps.targetTargetTank()) and IsSpellInRange("dark command", "mouseover") == 1, "mouseover"},
+			{ "dark command", 			IsShiftKeyDown() ~= nil and (targetThreatStatus ~= 3 or not jps.targetTargetTank()) and IsSpellInRange("dark command", "target") == 1, "target"},
+			{ "death grip", 			IsShiftKeyDown() ~= nil and jps.LastCast ~= "dark command" and (targetThreatStatus ~= 3 or not jps.targetTargetTank()) and IsSpellInRange("death grip", "mouseover") == 1, "mouseover"},
+			{ "death grip", 			IsShiftKeyDown() ~= nil and jps.LastCast ~= "dark command" and (targetThreatStatus ~= 3 or not jps.targetTargetTank()) and IsSpellInRange("death grip", "target") == 1, "target"},
+			{ "gorefiend's grasp", 		jps.LastCast == "death grip" },
+			-- Kick
+			{ "mind freeze", 			jps.shouldKick() },
+			{ "Strangulate", 			jps.shouldKick() and jps.LastCast ~= "mind freeze" },
+			
+			-- Defensive cooldowns
+			{ "Raise Dead", 			myHP < 0.4 and UnitExists("pet") == nil },
+			{ "Death Pact", 			myHP < 0.4 and haveGhoul},
+			{ "Icebound Fortitude", 	myHP < 0.5 },
+			{ "Vampiric Blood", 		myHP < 0.5 },
+			{ "Rune Tap", 				myHP < 0.6 },
+			-- cds
+			{ "Dancing Rune Weapon", 	jps.UseCDs },
+	      	{ jps.useTrinket(1),    	jps.UseCds },
+	      	{ jps.useTrinket(2),    	jps.UseCds },		
+			-- Buffs
+			{ "Bone Shield", 			not jps.buff("bone shield") },
+			-- Single target
+			{ "outbreak",				ffDuration <= 2 or bpDuration <= 2 },
+			{ "plague strike", 			not jps.debuff("blood plague") },
+			{ "icy touch", 				not jps.debuff("frost fever") },
+			{ "pestilence", 			IsShiftKeyDown() ~= nil},
+			{ "blood boil", 			"onCD"},
+			{ "death strike", 			"onCD"},
+			{ "rune strike", 			rp >= 40 },
+			{ "horn of winter", 		"onCD"},
+			{ "empower rune weapon" , 	not two_dr and not two_fr and not two_ur },
+			{ {"macro","/startattack"}, nil, "target" },
+	   	}
+	else
+		local spellTable =
+		{
+			-- Blood presence
+			{ "blood presence", 		not jps.buff("blood presence") },
+			
+			{ "blood tap", 				"onCD" },
+			
+			-- Moved DnD to the top so it will cast immediately
+			{ "death and decay",		IsShiftKeyDown() ~= nil and GetCurrentKeyBoardFocus() == nil }, -- GetCurrentKeyBoardFocus: Avoid casting while chat is open and you press shift
+			
+			{ "army of the dead",		IsLeftControlKeyDown() ~= nil and GetCurrentKeyBoardFocus() == nil }, -- todo
+			-- Taunt
+			{ "dark command", 			IsShiftKeyDown() ~= nil and (targetThreatStatus ~= 3 or not jps.targetTargetTank()) and IsSpellInRange("dark command", "mouseover") == 1, "mouseover"},
+			{ "dark command", 			IsShiftKeyDown() ~= nil and (targetThreatStatus ~= 3 or not jps.targetTargetTank()) and IsSpellInRange("dark command", "target") == 1, "target"},
+			{ "death grip", 			IsShiftKeyDown() ~= nil and jps.LastCast ~= "dark command" and (targetThreatStatus ~= 3 or not jps.targetTargetTank()) and IsSpellInRange("death grip", "mouseover") == 1, "mouseover"},
+			{ "death grip", 			IsShiftKeyDown() ~= nil and jps.LastCast ~= "dark command" and (targetThreatStatus ~= 3 or not jps.targetTargetTank()) and IsSpellInRange("death grip", "target") == 1, "target"},
+			-- Kick
+			{ "mind freeze", 			jps.shouldKick() },
+			{ "Strangulate", 			jps.shouldKick() and jps.LastCast ~= "mind freeze" },
+			
+			-- Defensive cooldowns
+			{ "Raise Dead", 			myHP < 0.4 and UnitExists("pet") == nil },
+			{ "Death Pact", 			myHP < 0.4 and haveGhoul},
+			{ "Icebound Fortitude", 	myHP < 0.5 },
+			{ "Vampiric Blood", 		myHP < 0.5 },
+			{ "Rune Tap", 				myHP < 0.6 },
+			-- cds
+			{ "Dancing Rune Weapon", 	jps.UseCDs },
+	      	{ jps.useTrinket(1),    	jps.UseCds },
+	      	{ jps.useTrinket(2),    	jps.UseCds },		
+			-- Buffs
+			{ "Bone Shield", 			not jps.buff("bone shield") },
+			-- Single target
+			{ "outbreak",				ffDuration <= 2 or bpDuration <= 2 },
+			{ "plague strike", 			not jps.debuff("blood plague") },
+			{ "icy touch", 				not jps.debuff("frost fever") },
+			{ "death strike", 			"onCD"},
+			{ "blood boil", 			jps.buff("crimson scourge") },
+			{ "soul reaper",			jps.hp("target") <= 0.35 },
+			{ "heart strike", 			jps.debuff("blood plague") and jps.debuff("frost fever") },
+			{ "rune strike", 			rp >= 40 },
+			{ "horn of winter", 		"onCD"},
+			{ "empower rune weapon" , 	not two_dr and not two_fr and not two_ur },
+			{ {"macro","/startattack"}, nil, "target" },
+	   	}
+	end
+	
 
    local spell,target = parseSpellTable(spellTable)
    jps.Target = target
--- rcjps/Rotations/paladin_protadin.lua
+++ rcjps/Rotations/paladin_protadin.lua
@@ -1,90 +1,175 @@
 function paladin_protadin(self)
-   --Gocargo
-   
-   if UnitCanAttack("player","target")~=1 or UnitIsDeadOrGhost("target")==1 then return end
+	--Gocargo
 
-   local myHealthPercent = UnitHealth("player")/UnitHealthMax("player") * 100
-   local targetHealthPercent = UnitHealth("target")/UnitHealthMax("target") * 100
-   local myManaPercent = UnitMana("player")/UnitManaMax("player") * 100
-   local hPower = UnitPower("player","9")
-   local stacks = jps.debuffStacks("Censure","target")
-   local spell = nil
-   local nStance = GetShapeshiftForm(nil);
-   local Acharge = jps.buffStacks("Bastion of Glory")
-   local targetdistance = CheckInteractDistance("target", 3)
-   local mythreat = UnitThreatSituation("player", "target")  
+	if UnitCanAttack("player","target")~=1 or UnitIsDeadOrGhost("target")==1 then return end
 
-    spellTable =
-   {    
-      -- Check for Which Seal to use
-       { "Seal of Truth",          	       not jps.MultiTarget and   nStance ~= 1 and stacks < 5 }, 
-      --{ "Seal of Insight",                not jps.MultiTarget and jps.Defensive and nStance ~= 3 },
-       { "Seal of Righteousness",          jps.MultiTarget  and nStance ~= 2 }, 
-       { "Seal of Insight",                nStance ~= 3 and stacks == 5	and not jps.MultiTarget						 },
-       { "Word of Glory", 				   IsAltKeyDown() ~= nil }, 
-      -- { "Cleanse", 					  jps.Defensive },
-       { "Shield of the Righteous",				 IsShiftKeyDown() ~= nil   },
-        { "Divine Protection",              IsShiftKeyDown() ~= nil and jps.LastCast ~= "Shield of the Righteous" },
-      -- { "Cleanse", 						    IsControlKeyDown() ~= nil },
-       { "Holy Avenger",                    jps.UseCDs				 },
-       
-       
-             -- Kicks
-      { "Rebuke",                         jps.shouldKick() },
-      { "Rebuke",                         jps.shouldKick("focus"), "focus" },
-      { "Arcane Torrent",                 jps.shouldKick() and IsSpellInRange("Crusader Strike","target")==1 and jps.LastCast ~= "Rebuke" },
-      { "Avenger's Shield",               jps.shouldKick() and ((jps.LastCast ~= "Rebuke") or (jps.LastCast ~= "Arcane Torrent")) },
-      { "Hammer of Wrath",                },
-        { "Shield of the Righteous",         hPower >= 5 },
-       { "Hammer of the Righteous",       not jps.debuff("Weakened Blows", "target") or jps.MultiTarget }, 
-       { "Crusader Strike",                },
-       { "Avenger's Shield",               jps.buff("Grand Crusader") },
-       { "Judgment",                       },
-       { "Avenger's Shield",              not jps.Interrupts  },   
-       
-      -- Defensive Cooldowns
-     -- { "Lay on Hands",                   myHealthPercent < 10  }, --10 Minute CD
-      --{ "Guardian of Ancient Kings",      myHealthPercent < 25  }, --3 Minute CD
-      { "Ardent Defender",                myHealthPercent < 15  }, --3 Minute CD
-     -- { "Seal of Insight",                (myHealthPercent < 50 or myManaPercent < 40 ) and nStance ~= 3 },
-      --1 Minute CD
+	local myHealthPercent = UnitHealth("player")/UnitHealthMax("player") * 100
+	local targetHealthPercent = UnitHealth("target")/UnitHealthMax("target") * 100
+	local myManaPercent = UnitMana("player")/UnitManaMax("player") * 100
+	local hPower = UnitPower("player","9")
+	local stacks = jps.debuffStacks("Censure","target")
+	local censure_time_left = jps.debuffDuration("Censure","target")
+	local spell = nil
+	local nStance = GetShapeshiftForm(nil);
+	local Acharge = jps.buffStacks("Bastion of Glory")
+	local targetdistance = CheckInteractDistance("target", 3)
+	local mythreat = UnitThreatSituation("player", "target")  
 
-      --Active Mitgations
-      --{ "Word of Glory",                  myHealthPercent < 75 and jps.buff("Shield of the Righteous") },
-      --{ "Word of Glory",                  myHealthPercent < 40 and Acharge >= 4  },
-      
-     
+	if(jps.MultiTarget) then
+		--aoe rotation
+		spellTable =
+		{  
+			
+			-- Basic Setup Stuff
+			{ "Righteous Fury",                 not jps.buff("Righteous Fury") },
+			
+			-- "Oh shit" cd usage
+			{ "Lay on Hands",              		myHealthPercent < 10  }, --10 Minute CD
+			{ "Guardian of Ancient Kings",  	myHealthPercent < 25  }, --3 Minute CD
+			{ "Ardent Defender",                myHealthPercent < 15  }, --3 Minute CD
+			--1 Minute CD
+	
+			--Active Healing
+			{ "Word of Glory",            		hPower >= 3 and myHealthPercent < 75 and Acharge >= 5 },
+			{ "Eternal Flame",            		hPower >= 3 and myHealthPercent < 75 and Acharge >= 5 },
+			{ "Word of Glory",            		hPower >= 3 and myHealthPercent < 40  },
+			{ "Eternal Flame",            		hPower >= 3 and myHealthPercent < 40  },
+			
+			
+			  
+			-- Check Seal
+			{ "Seal of Righteousness",       	nStance ~= 2 },
+			
+			-- Cleanse
+			-- { "Cleanse", 					jps.Defensive },
+			-- CDs
+			{ "Avenging Wrath",             	jps.UseCDs },
+			{ jps.useTrinket(1),      		  	jps.UseCDs },
+			{ jps.useTrinket(2),       		  	jps.UseCDs },
+			{ "Execution Sentence",             "onCD"},
+			{ "Divine Protection",              jps.Defensive and myHealthPercent < 99 },
+			{ "Holy Avenger",           		 myHealthPercent < 35 and jps.UseCDs},
+	
+	
+			-- Kicks
+			{ "Rebuke",                         jps.shouldKick() },
+			{ "Rebuke",                         jps.shouldKick("focus"), "focus" },
+			{ "Arcane Torrent",                 jps.shouldKick() and IsSpellInRange("Crusader Strike","target")==1 and jps.LastCast ~= "Rebuke" },
+			{ "Avenger's Shield",               jps.shouldKick() and ((jps.LastCast ~= "Rebuke") or (jps.LastCast ~= "Arcane Torrent")) },
+			
+			-- Holy Power Usage
+			{ "Eternal Flame",       			hPower >= 3 and not jps.buff("Eternal Flame") },
+			{ "Shield of the Righteous",       	hPower >= 3 },
+			
+			-- Proc usage
+			{ "Avenger's Shield",               jps.buff("Grand Crusader") },
+			
+			-- Main attack priority
+			{ "Holy Wrath",                     "onCD"},
+			{ "Hammer of the Righteous",        "onCD"},
+			{ "Consecration",                   IsSpellInRange("Crusader Strike","target")==1},
+			{ "Judgment",                       "onCD"},
+			{ "Avenger's Shield",               "onCD"},  
+			{ "Holy Prism",						"onCD"},
+			{ "Hammer of Wrath",                "onCD"},
+			
+			
+	
+			
+	
+			{ "Sacred Shield",                 	jps.buff("Sacred Shield")},
+			{ "Hand of Purity",               	jps.Defensive			   	 },
+	
+			--{ "Light's Hammer",             	IsShiftKeyDown() ~= nil and GetCurrentKeyBoardFocus() == nil },
+	
+			-- CDs
+	
+			-- { "Avenging Wrath",             	jps.UseCDs },
+			{ jps.useTrinket(1),      		  	jps.UseCDs },
+			{ jps.useTrinket(2),       		  	jps.UseCDs },
+			{ "Execution Sentence",             			 },    
+			
+			{ {"macro","/startattack"}, 		nil, "target" },
+		}
+	else
+		-- single target rotation
+		spellTable =
+		{  
+			
+			-- Basic Setup Stuff
+			{ "Righteous Fury",                 not jps.buff("Righteous Fury") },
+			
+			-- "Oh shit" cd usage
+			{ "Lay on Hands",              		myHealthPercent < 10  }, --10 Minute CD
+			{ "Guardian of Ancient Kings",  	myHealthPercent < 25  }, --3 Minute CD
+			{ "Ardent Defender",                myHealthPercent < 15  }, --3 Minute CD
+			--1 Minute CD
+	
+			--Active Healing
+			{ "Word of Glory",            		hPower >= 3 and myHealthPercent < 75 and Acharge >= 5 },
+			{ "Eternal Flame",            		hPower >= 3 and myHealthPercent < 75 and Acharge >= 5 },
+			{ "Word of Glory",            		hPower >= 3 and myHealthPercent < 40  },
+			{ "Eternal Flame",            		hPower >= 3 and myHealthPercent < 40  },
+			
+			
+			  
+			-- Check for Which Seal to use
+			{ "Seal of Insight",                nStance ~= 3},
+			
+			-- Cleanse
+			-- { "Cleanse", 					jps.Defensive },
+			-- CDs
+			{ "Avenging Wrath",             	jps.UseCDs },
+			{ jps.useTrinket(1),      		  	jps.UseCDs },
+			{ jps.useTrinket(2),       		  	jps.UseCDs },
+			{ "Execution Sentence",             "onCD"},
+			{ "Divine Protection",              jps.Defensive and myHealthPercent < 99 },
+			{ "Holy Avenger",           		jps.UseCDs},
+	
+	
+			-- Kicks
+			{ "Rebuke",                         jps.shouldKick() },
+			{ "Rebuke",                         jps.shouldKick("focus"), "focus" },
+			{ "Arcane Torrent",                 jps.shouldKick() and IsSpellInRange("Crusader Strike","target")==1 and jps.LastCast ~= "Rebuke" },
+			{ "Avenger's Shield",               jps.shouldKick() and ((jps.LastCast ~= "Rebuke") or (jps.LastCast ~= "Arcane Torrent")) },
+			
+			-- Holy Power Usage
+			{ "Eternal Flame",       			hPower >= 3 and not jps.buff("Eternal Flame") },
+			{ "Shield of the Righteous",       	hPower >= 3 },
+			
+			-- Proc usage
+			{ "Avenger's Shield",               jps.buff("Grand Crusader") },
+			
+			-- Main attack priority
+			{ "Crusader Strike",                "onCD"},
+			{ "Judgment",                       "onCD"},
+			{ "Avenger's Shield",               "onCD"},  
+			{ "Holy Prism",						"onCD"},
+			{ "Holy Wrath",                     "onCD"},
+			{ "Hammer of Wrath",                "onCD"},
+			{ "Consecration",                   IsSpellInRange("Crusader Strike","target")==1},
+			
+	
+			
+	
+			{ "Sacred Shield",                 	jps.buff("Sacred Shield")},
+			{ "Hand of Purity",               	jps.Defensive			   	 },
+	
+			--{ "Light's Hammer",             	IsShiftKeyDown() ~= nil and GetCurrentKeyBoardFocus() == nil },
+	
+			-- CDs
+	
+			-- { "Avenging Wrath",             	jps.UseCDs },
+			{ jps.useTrinket(1),      		  	jps.UseCDs },
+			{ jps.useTrinket(2),       		  	jps.UseCDs },
+			{ "Execution Sentence",             			 },    
+			
+			{ {"macro","/startattack"}, 		nil, "target" },
+		}
+	end
+	
 
-
-      
-      -- Basic Setup Stuff
-      { "Righteous Fury",                 not jps.buff("Righteous Fury") },
-     
-     
-     
-      { "Sacred Shield",                   jps.Defensive and not jps.buff("Sacred Shield")				 },
-      { "Hand of Purity",                  jps.Defensive			   	 },
-       
-      --{ "Light's Hammer",                 IsShiftKeyDown() ~= nil and GetCurrentKeyBoardFocus() == nil },
-      
-      -- CDs
-     
-     -- { "Avenging Wrath",                 jps.UseCDs },
-      { jps.useTrinket(1),      		  jps.UseCDs },
-      { jps.useTrinket(2),       		  jps.UseCDs },
-      { "Execution Sentence",             			 },    
-      -- Damage
-     
-     
-      { "Consecration",                   jps.MultiTarget and IsSpellInRange("Crusader Strike","target")==1 }, 
-      
-    
-      { "Holy Wrath",                     },
-
-      { {"macro","/startattack"}, nil, "target" },
-   }
-
-   local spell,target = parseSpellTable(spellTable)
-   jps.Target = target
-   return spell
+	local spell,target = parseSpellTable(spellTable)
+	jps.Target = target
+	return spell
 end
